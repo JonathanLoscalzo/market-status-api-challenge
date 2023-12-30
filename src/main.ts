d58fc4c86159f695f2536398a6b1ced043085f76
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2023-12-30 17:27:19 -0300

    chore: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as df from 'dotenv-flow';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './core/exception-filters/http/http.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   df.config({
@@ -11,7 +14,32 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  addFilters(app);
+  addPipes(app);
+  addSwagger(app);
+
   const configService = app.get(ConfigService);
   await app.listen(configService.get('port'));
 }
+
+function addFilters(app) {
+  app.useGlobalFilters(new HttpExceptionFilter());
+}
+
+function addSwagger(app) {
+  // TODO: make it public just for dev
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Market Status API')
+    .setDescription('Endpoints for Market Status API')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+}
+
+function addPipes(_app: INestApplication<any>) {
+  _app;
+}
+
 bootstrap();
